21496540d7886de72e7b98870f9b6de0964bce25
taroj khan <tarujkhan@yahoo.com>
2021-02-18 19:30:59 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-
+  resources :artists do 
+    resources :songs, only: [:index, :show, :new]
   namespace :admin do
     resources :preferences, only: [:index, :show, :new]
   end
